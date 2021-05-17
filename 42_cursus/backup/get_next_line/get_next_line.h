4cb10c56a16906d8c92d0caa80a8fe9b149c1d09
MinHoon-LEE <lucky3749@naver.com>
2021-05-17 20:52:43 +0900

    2021-05-17

--- 42_cursus/backup/get_next_line/get_next_line.h
+++ 42_cursus/backup/get_next_line/get_next_line.h
@@ -10,17 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINES_H
-# define GET_NEXT_LINES_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
 
-# define BUFFER_SIZE 32
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5000
+# endif
 
-int get_next_line(int fd, char **line);
+# ifndef OPEN_MAX
+#  define OPEN_MAX 32
+# endif
 
+int     check_newline(char *arr);
 
-int	do_read(int fd, char *str);
+int     cut_line(char **storage, int index, char **line);
+
+int     check_storage(char **storage, int read_size, char **line);
+
+int     get_next_line(int fd, char **line);
+
+size_t  ft_strlen(const char *str);
+
+char    *ft_strdup(const char *src);
+
+size_t  ft_strlcpy(char *dest, const char *src, size_t size);
+
+size_t  ft_strlcat(char *dest, const char *src, size_t size);
+
+char    *ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- 42_cursus/get_next_line/get_next_line.h
+++ 42_cursus/get_next_line/get_next_line.h
@@ -1,1 +1,47 @@
-#
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_lines.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minlee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/13 19:57:54 by minlee            #+#    #+#             */
+/*   Updated: 2021/05/13 20:02:03 by minlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5000
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 32
+# endif
+
+int     check_newline(char *arr);
+
+int     cut_line(char **storage, int index, char **line);
+
+int     check_storage(char **storage, int read_size, char **line);
+
+int     get_next_line(int fd, char **line);
+
+size_t  ft_strlen(const char *str);
+
+char    *ft_strdup(const char *src);
+
+size_t  ft_strlcpy(char *dest, const char *src, size_t size);
+
+size_t  ft_strlcat(char *dest, const char *src, size_t size);
+
+char    *ft_strjoin(char const *s1, char const *s2);
+
+#endif
